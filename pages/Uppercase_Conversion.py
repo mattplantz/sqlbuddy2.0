b729a440d246228b011021d7aaab61ab8d529d7e
mattplantz <118403738+mattplantz@users.noreply.github.com>
2023-05-17 16:28:09 -0500

    Update Uppercase_Conversion.py
    reverted new line characters back to lowercase

--- pages/Uppercase_Conversion.py
+++ pages/Uppercase_Conversion.py
@@ -5,7 +5,12 @@ st.subheader('Please paste the text you want to conver to uppercase in the textb
 
 col1, col2 = st.columns(2, gap ='medium')
 
+def upper_fmt(t):
+    t = t.upper()
+    t.replace('\\N', '\\n')
+    return t
+
 with col1:
     txt = st.text_area('Text to Convert', '')
 with col2:
-    st.code(txt.upper(), language = "sql")
+    st.code(upper_fmt(txt), language = "sql")
