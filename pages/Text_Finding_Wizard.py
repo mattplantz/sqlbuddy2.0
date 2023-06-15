36491a3e93baf7cb96a56d89353a7a32176d08ba
mattplantz <118403738+mattplantz@users.noreply.github.com>
2023-06-15 13:31:57 -0500

    Create Text_Finding_Wizard.py
    adding text finding wizard

---
+++ pages/Text_Finding_Wizard.py
@@ -0,0 +1,51 @@
+"""
+Created on Thu Jun 15 12:01:49 2023
+
+@author: mplantz
+
+Goal: Search a string of text for a specific supstring
+"""
+
+import streamlit as st
+
+st.header("Text Finding Wizard")
+st.subheader('Please input both the whole text you want to search and the text you hope to find.')
+
+def exists(srch, fnd):
+    start = srch.find(fnd)
+    if start == -1:
+        return 'String does not exist in search :('
+    else:
+        return 'String exists in search :)'
+    
+def search(srch, fnd, length):
+    start = srch.find(fnd)
+    ln = len(fnd)
+    end = start+ln
+    start_extended = start - length
+    end_extended = end + length
+    if start == -1:
+        return 'String does not exist in search :('
+    else:
+        return srch[start_extended:end_extended]
+
+col1, col2 = st.columns(2, gap = 'medium')
+
+with col1:
+    all_text = st.text_area('Text to SEARCH','')
+with col2:
+    find = st.text_area('Text to FIND','')
+
+
+with st.sidebar:
+    kind = st.radio('What would you like to do?',['Check if search exists','Extract text surrounding match'])
+    lng = st.number_input(label = 'Number of characters around match to return'
+                          , min_value = 0
+                          , max_value = 100
+                          , value = 0)
+
+
+if kind == 'Check if search exists':
+    st.write(exists(all_text, find))
+if kind == 'Extract text surrounding match':
+    st.write(search(all_text, find, lng))
